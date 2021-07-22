50eba2347ed8313d2472fe98c2eb07253867705f
Caio Vieira <caiocesarav0208@gmail.com>
2021-07-22 10:48:18 -0300

    feat: :sparkles: implement react-router-dom
    set react router dom on App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { Home } from './pages/Home'
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
