eb07ff98818feee3db29e513df35162c17f15740
Caio Vieira <caiocesarav0208@gmail.com>
2021-07-23 16:07:07 -0300

    feat: :sparkles: implement planets page
    make planets page show all planets

--- src/pages/Planets/index.tsx
+++ src/pages/Planets/index.tsx
@@ -1,8 +1,58 @@
 import { Container } from './styles'
+import { Card } from '../../components/Card'
+import { Loading } from '../../components/Load'
+import { useEffect, useState } from 'react'
+import { Planets as SWPlanets } from 'swapi-ts'
+
+type PlanetType = {
+    name: string;
+    population: string;
+    terrain: string;
+    id: string;
+}
 
 export function Planets() {
+    const [planetList, setPlanetList] = useState<PlanetType[]>();
+
+    useEffect(() => {
+        async function apiCall() {
+            //get all planets
+            const planets = await SWPlanets.find();
+            //get card info
+            const cardInfo = planets.resources.map(planet => {
+                return {
+                    name: planet.value.name,
+                    population: planet.value.population,
+                    terrain: planet.value.terrain,
+                    id: planet.value.url
+                }
+            })
+            setPlanetList(cardInfo)
+
+        }
+
+        apiCall();
+    }, [])
 
     return (
-        <Container></Container>
+        <Container>
+            {planetList ?
+                planetList.map(planet =>
+                    <Card
+                        field1="Nome:"
+                        info1={planet.name}
+                        field2="População:"
+                        info2={planet.population}
+                        field3="Terreno:"
+                        info3={planet.terrain}
+                        photo={planet.id}
+                        cardType="planet"
+
+                    />) :
+
+                <Loading />
+            }
+
+        </Container>
     )
 }
