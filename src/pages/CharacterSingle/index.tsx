6291c04ff5e72b6f1ca72578c016835287f715a1
Caio Vieira <caiocesarav0208@gmail.com>
2021-07-23 12:47:19 -0300

    feat: :sparkles: include character photo
    implement character photo and improve responsive

--- src/pages/CharacterSingle/index.tsx
+++ src/pages/CharacterSingle/index.tsx
@@ -1,21 +1,89 @@
-import { Container, Field, Info, Line } from './styles'
+import error from '../../assets/erro.jpg'
+import { Container, Field, Info, Line, Photo, Text } from './styles'
 import { useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react';
 
-import { People, Species, Starships, Films, Vehicles, Planets } from 'swapi-ts'
+import {
+    People,
+    Species,
+    Starships,
+    Films,
+    Vehicles,
+    Planets, ISpecie, IStarship, IFilm, IVehicle, IPlanet
+} from 'swapi-ts'
+
+type CharacterDataType = {
+    name: string;
+    height: string;
+    films: string;
+    homeworld: string;
+    mass: string;
+    species: string;
+    starships: string;
+    vehicles: string;
+    gender: string;
+    birth: string;
+}
 
 export function CharacterSingle() {
     const params = useParams<{ name: string }>();
 
-    const [characterData, setCharacterData] = useState<any>()
+    const [characterData, setCharacterData] = useState<CharacterDataType>()
+    const [photo, setPhoto] = useState()
+
+    async function findCharacter(name: string) {
+        return (await People.findBySearch([name])).resources.map(entry => entry.value)
+    }
+    async function findSpecieName(specieUrl: string | ISpecie) {
+        return (await Species.find(specie => specie.url === specieUrl)).resources.map(entry => {
+            if (entry.value.url === specieUrl) {
+                return entry.value.name
+            }
+        });
+    }
+    async function findStarshipsNames(starshipsUrlArray: string[] | IStarship[]) {
+        return (await Starships.find(ship => {
+            for (let i = 0; i < starshipsUrlArray.length; i++) {
+                if (ship.url === starshipsUrlArray[i]) {
+                    return true;
+                }
+            }
+            return false;
+
+        })).resources.map(entry => entry.value.name)
+    }
+    async function findFilmsNames(filmsUrlArray: string[] | IFilm[]) {
+        return (await Films.find(film => {
+            for (let i = 0; i < filmsUrlArray.length; i++) {
+                if (film.url === filmsUrlArray[i]) {
+                    return true;
+                }
+            }
+            return false;
+        })).resources.map(entry => entry.value.title)
+    }
+    async function findVehiclesNames(vehiclesUrlArray: string[] | IVehicle[]) {
+        return (await Vehicles.find(vehicle => {
+            for (let i = 0; i < vehiclesUrlArray.length; i++) {
+                if (vehicle.url === vehiclesUrlArray[i]) {
+                    return true;
+                }
+            }
+            return false;
+        })).resources.map(entry => entry.value.name)
+    }
+    async function findHomeworldName(planetUrl: string | IPlanet) {
+        return (await Planets.find(world => world.url === planetUrl)).resources.map(entry => entry.value.name)
+    }
 
     useEffect(() => {
         async function apiCall() {
-            let char = {} as any;
-            const character = await (await People.findBySearch([params.name])).resources.map(entry => entry.value);
+            let char = {} as CharacterDataType;
+            const character = await findCharacter(params.name);
             console.log(character)
             if (character.length !== 1) {
                 //erro de busca, numero de personagens invalido
+                //TODO criar 404
                 console.error("Invalid search parameter")
                 return;
             }
@@ -25,70 +93,47 @@ export function CharacterSingle() {
             char.gender = character[0].gender === 'male' ? "masculino" : character[0].gender === 'female' ? 'feminino' : character[0].gender;
             char.mass = character[0].mass;
 
-
             if (character[0].species.length >= 1) {
-                const species = await (await Species.find(specie => specie.url === character[0].species[0])).resources.map(entry => {
-                    if (entry.value.url === character[0].species[0]) {
-                        return entry.value.name
-                    }
-                });
-                console.log(species)
-                char.species = species
+                const species = await findSpecieName(character[0].species[0])
+                char.species = species.join(', ')
             } else {
                 char.species = "desconhecido"
             }
             if (character[0].starships.length >= 1) {
-                const ships = await (await Starships.find(ship => {
-                    for (let i = 0; i < character[0].starships.length; i++) {
-                        if (ship.url === character[0].starships[i]) {
-                            return true;
-                        }
-                    }
-                    return false;
-
-                })).resources.map(entry => entry.value.name)
-                console.log(ships)
+                const ships = await findStarshipsNames(character[0].starships)
                 char.starships = ships.join(', ')
             } else {
                 char.starships = "não possui"
             }
             if (character[0].films.length >= 1) {
-                const films = await (await Films.find(film => {
-                    for (let i = 0; i < character[0].films.length; i++) {
-                        if (film.url === character[0].films[i]) {
-                            return true;
-                        }
-                    }
-                    return false;
-                })).resources.map(entry => entry.value.title)
-                console.log(films)
+                const films = await findFilmsNames(character[0].films)
                 char.films = films.join(', ')
             } else {
                 char.films = "sem participações"
             }
             if (character[0].vehicles.length >= 1) {
-                const vehicles = await (await Vehicles.find(vehicle => {
-                    for (let i = 0; i < character[0].vehicles.length; i++) {
-                        if (vehicle.url === character[0].vehicles[i]) {
-                            return true;
-                        }
-                    }
-                    return false;
-                })).resources.map(entry => entry.value.name)
-                console.log(vehicles)
+                const vehicles = await findVehiclesNames(character[0].vehicles)
                 char.vehicles = vehicles.join(', ')
             } else {
                 char.vehicles = "não possui"
             }
             if (character[0].homeworld) {
-                const homeworld = await (await Planets.find(world => world.url === character[0].homeworld)).resources.map(entry => entry.value.name)
-                console.log(homeworld)
-                char.homeworld = homeworld
+                const homeworld = await findHomeworldName(character[0].homeworld)
+
+                char.homeworld = homeworld.join(', ')
             } else {
                 char.homeworld = 'desconhecido'
             }
 
             setCharacterData(char)
+            const charId = character[0].url.replace(/\/$/g, "");
+            const photoId = charId.substring(charId.lastIndexOf('/') + 1);
+            import(`../../assets/characters/${photoId}.jpg`)
+                .catch(() => {
+                }).then((res) => {
+                    //set photoSrc
+                    setPhoto(res.default)
+                })
         }
         apiCall()
     }, [params.name])
@@ -96,46 +141,49 @@ export function CharacterSingle() {
     return (
         <Container>
             {characterData && <>
-                <Line>
-                    <Field>Nome:</Field>
-                    <Info> {characterData.name}</Info>
-                </Line>
-                <Line>
-                    <Field>Genero:</Field>
-                    <Info> {characterData.gender}</Info>
-                </Line>
-                <Line>
-                    <Field>Ano de nascimento:</Field>
-                    <Info> {characterData.birth}</Info>
-                </Line>
-                <Line>
-                    <Field>Altura:</Field>
-                    <Info> {characterData.height}cm</Info>
-                </Line>
-                <Line>
-                    <Field>Peso:</Field>
-                    <Info> {characterData.mass}Kg</Info>
-                </Line>
-                <Line>
-                    <Field>Planeta natal:</Field>
-                    <Info> {characterData.homeworld}</Info>
-                </Line>
-                <Line>
-                    <Field>Espécie:</Field>
-                    <Info> {characterData.species}</Info>
-                </Line>
-                <Line>
-                    <Field>Naves espaciais:</Field>
-                    <Info> {characterData.starships}</Info>
-                </Line>
-                <Line>
-                    <Field>Filmes:</Field>
-                    <Info> {characterData.films}</Info>
-                </Line>
-                <Line>
-                    <Field>Veículos:</Field>
-                    <Info> {characterData.vehicles}</Info>
-                </Line>
+                <Photo src={photo || error} />
+                <Text>
+                    <Line>
+                        <Field>Nome:</Field>
+                        <Info> {characterData.name}</Info>
+                    </Line>
+                    <Line>
+                        <Field>Genero:</Field>
+                        <Info> {characterData.gender}</Info>
+                    </Line>
+                    <Line>
+                        <Field>Ano de nascimento:</Field>
+                        <Info> {characterData.birth}</Info>
+                    </Line>
+                    <Line>
+                        <Field>Altura:</Field>
+                        <Info> {characterData.height}cm</Info>
+                    </Line>
+                    <Line>
+                        <Field>Peso:</Field>
+                        <Info> {characterData.mass}Kg</Info>
+                    </Line>
+                    <Line>
+                        <Field>Planeta natal:</Field>
+                        <Info> {characterData.homeworld}</Info>
+                    </Line>
+                    <Line>
+                        <Field>Espécie:</Field>
+                        <Info> {characterData.species}</Info>
+                    </Line>
+                    <Line>
+                        <Field>Naves espaciais:</Field>
+                        <Info> {characterData.starships}</Info>
+                    </Line>
+                    <Line>
+                        <Field>Filmes:</Field>
+                        <Info> {characterData.films}</Info>
+                    </Line>
+                    <Line>
+                        <Field>Veículos:</Field>
+                        <Info> {characterData.vehicles}</Info>
+                    </Line>
+                </Text>
 
             </>}
 
--- src/pages/CharacterSingle/styles.ts
+++ src/pages/CharacterSingle/styles.ts
@@ -1,6 +1,27 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+position: relative;
+left: 50%;
+transform:translateX(-50%);
+margin-top: 56px;
+width: 90%;
+
+padding:48px 24px;
+background-color:black;
+border: 2px solid hsl(45,89%,45%);
+border-radius: 8px;
+
+display: flex;
+gap: 24px;
+
+@media(max-width:1024px){
+    flex-direction: column;
+    align-items: center;
+}
+
+`
+export const Text = styled.div`
 display: flex;
 flex-direction: column;
 gap: 28px;
@@ -15,4 +36,9 @@ export const Info = styled.span`
 color: hsl(45,89%,45%);
 font-size: 24px;
 text-transform: capitalize;
+`
+export const Photo = styled.img`
+width:403px;
+height:554px;
+border-radius: 8px;
 `
