a80be1a8d1918adb0dea3d13c731afd62b4d446e
Caio Vieira <caiocesarav0208@gmail.com>
2021-07-23 09:28:40 -0300

    feat: :construction: prepare card for photo
    create img element and position it to make room for photos

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,27 +1,31 @@
-import { Container, Field, Info, Line } from './styles'
+import { Container, Field, Info, Line, Photo, Text } from './styles'
 
 type CardProps = {
     name: string;
     planet: string;
     birth: string;
+    photo?: string;
 }
 
 export function Card(props: CardProps) {
 
     return (
         <Container>
-            <Line>
-                <Field>Nome: </Field>
-                <Info> {props.name}</Info>
-            </Line>
-            <Line>
-                <Field>Planeta: </Field>
-                <Info> {props.planet}</Info>
-            </Line>
-            <Line>
-                <Field>Nascimento: </Field>
-                <Info> {props.birth}</Info>
-            </Line>
+            <Photo src={props.photo ? props.photo : ""} />
+            <Text>
+                <Line>
+                    <Field>Nome: </Field>
+                    <Info> {props.name}</Info>
+                </Line>
+                <Line>
+                    <Field>Planeta: </Field>
+                    <Info> {props.planet}</Info>
+                </Line>
+                <Line>
+                    <Field>Nascimento: </Field>
+                    <Info> {props.birth}</Info>
+                </Line>
+            </Text>
         </Container>
     )
 }
--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -1,23 +1,34 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-padding: 30px;
+padding: 20px;
 background-color:black;
 border: 2px solid hsl(45,89%,45%);
 border-radius: 8px;
+
+width: 426px;
+display: flex;
+gap: 20px;
+align-items: center;
+`
+export const Text = styled.div`
 display: flex;
 flex-direction:column;
 gap: 28px;
-width: 400px;
 `
 export const Line = styled.p``
 export const Field = styled.strong`
-font-size: 24px;
+font-size: 20px;
 color: hsl(45,89%,45%);
 text-transform: capitalize;
 `
 export const Info = styled.span`
 color: hsl(45,89%,45%);
-font-size: 24px;
+font-size: 20px;
 text-transform: capitalize;
+`
+export const Photo = styled.img`
+height:155px;
+width:105px;
+border-radius: 8px;
 `
